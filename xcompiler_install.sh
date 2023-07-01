b32f4e6ca8c8c0deed6d68b48b113bcd44150b18
Spooghetti420 <kkovachev1947@gmail.com>
2023-07-01 15:27:00 +0100

    Fix sample command not being filled in

--- xcompiler_install.sh
+++ xcompiler_install.sh
@@ -1,6 +1,6 @@
 apt install libgmp3-dev libmpfr-dev libmpc-dev
-curl https://ftp.gnu.org/gnu/gcc/gcc-9.3.0/gcc-9.3.0.tar.gz --output gcc9.3.0.tar.gz
-curl https://ftp.gnu.org/gnu/binutils/binutils-2.34.tar.gz --ouput binutils-2.34.tar.gz
+# curl https://ftp.gnu.org/gnu/gcc/gcc-9.3.0/gcc-9.3.0.tar.gz --output gcc9.3.0.tar.gz
+# curl https://ftp.gnu.org/gnu/binutils/binutils-2.34.tar.gz --output binutils-2.34.tar.gz
 mkdir gcc-source
 tar -xf gcc9.3.0.tar.gz --directory gcc-source
 tar -xf binutils-2.34.tar.gz --directory gcc-source
@@ -10,7 +10,7 @@ export PATH="$PREFIX/bin:$PATH"
 cd gcc-source
 mkdir build-binutils
 cd build-binutils
-../binutils-x.y.z/configure --target=$TARGET --prefix="$PREFIX" --with-sysroot --disable-nls --disable-werror
+../binutils-2.34/configure --target=$TARGET --prefix="$PREFIX" --with-sysroot --disable-nls --disable-werror
 make -j6
 make install
 
