8a66e5c184ee1fd60f99894b1a7578dc10e0fc61
Spooghetti420 <kkovachev1947@gmail.com>
2023-07-02 21:01:08 +0100

    Add skeleton code from osdev.org/Bare_Bones

---
+++ learning/bare-bones/build.sh
@@ -0,0 +1,8 @@
+echo "Assembling bootstrap code..." & $HOME/opt/cross/bin/i686-elf-as boot.s -o boot.o
+echo "Compiling kernel..." & $HOME/opt/cross/bin/i686-elf-gcc -c kernel.c -o kernel.o -std=gnu99 -ffreestanding -O2 -Wall -Wextra
+echo "Linking bootstrap and kernel..." & $HOME/opt/cross/bin/i686-elf-gcc -T linker.ld -o myos.bin -ffreestanding -O2 -nostdlib boot.o kernel.o -lgcc
+grub-file --is-x86-multiboot myos.bin && echo "OS was built x86-bootable" || echo "OS cannot be booted on x86!"
+mkdir -p isodir/boot/grub
+cp myos.bin isodir/boot/myos.bin
+cp grub.cfg isodir/boot/grub/grub.cfg
+grub-mkrescue -o myos.iso isodir
---
+++ learning/bare-bones/run.sh
@@ -0,0 +1,2 @@
+sh build.sh # Build the program first
+qemu-system-i386 -cdrom myos.iso
