b58177006a8f2dfde913985e51d9beba75f9d27f
Spooghetti420 <kkovachev1947@gmail.com>
2023-07-02 21:04:31 +0100

    Update build scripts

--- learning/bare-bones/run.sh
+++ learning/bare-bones/run.sh
@@ -1,2 +1,3 @@
 sh build.sh # Build the program first
-qemu-system-i386 -cdrom myos.iso
+qemu-system-i386 -cdrom myos.iso # Run using QEMU, runs through the bootloader (GRUB) first
+# BE REALLY CAREFUL NOT TO CLICK INSIDE THE WINDOW, IT IS REALLY HARD TO REGAIN CONTROL OF YOUR COMPUTER AFTERWARDS
