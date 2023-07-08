0f6a5fc42a5e05e2bf17feee99aed305395997c1
Spooghetti420 <kkovachev1947@gmail.com>
2023-07-08 20:43:38 +0100

    Remove unnecessary lines

--- learning/meaty-skeleton/libc/math/abs.c
+++ learning/meaty-skeleton/libc/math/abs.c
@@ -1,15 +1,3 @@
-#include <limits.h>
-#include <stdio.h>
-#include <stdint.h>
-#include <stdlib.h>
-
-double fabs(double x) {
+double abs(double x) {
     return x >= 0 ? x : -x;
-}
-
-int main() {
-    for (int i = 0; i < 1000000; i++) {
-        double x = (double) rand();
-        printf("abs(%f)=%f\n", x, fabs(x));
-    }
 }
